d6b69eefe81ff6ea0219c71fd169e14eb782cb96
Oyedelehabeeb <hoyedeley4@gmail.com>
2024-10-29 08:18:18 +0100

    update path

--- src/Services/supabase.js
+++ src/Services/supabase.js
@@ -4,7 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 // const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 // const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabaseUrl = "https://zjcujdhxrekqqljhhlaf.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqY3VqZGh4cmVrcXFsamhobGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxODgyNTgsImV4cCI6MjA0Mjc2NDI1OH0.GDznTBOE8YZ6hgKmb6-H27hFVfA6OPJp3z0Xoi4Kmkg";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
